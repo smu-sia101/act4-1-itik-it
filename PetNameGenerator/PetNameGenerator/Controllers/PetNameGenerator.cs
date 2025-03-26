e3fcd92a3b01bb9b41fea232f9f96f419ca24335
IanSalvador Ramos <IanSalvadorRamos23@outlook.com>
2025-03-26 00:38:16 -0800

    Final

--- PetNameGenerator/PetNameGenerator/Controllers/PetNameGenerator.cs
+++ PetNameGenerator/PetNameGenerator/Controllers/PetNameGenerator.cs
@@ -4,15 +4,56 @@ namespace PetNameGenerator.Controllers
 {
     [ApiController]
     [Route("api/petnameGenerator")]
-    public class PetNameGenerator : ControllerBase
+    public class PetNameGeneratorController : ControllerBase
     {
-        private string[] petnameGenerator = new string[]
-        {
+        private static readonly string[] dogNames = { "Buddy", "Max", "Charlie", "Rocky", "Rex" };
+        private static readonly string[] catNames = { "Whiskers", "Mittens", "Luna", "Simba", "Tiger" };
+        private static readonly string[] birdNames = { "Tweety", "Sky", "Chirpy", "Raven", "Sunny" };
 
-        };
-        public IActionResult Index()
+        private static readonly Random _random = new Random();
+
+        [HttpPost("generate")]
+        public IActionResult GeneratePetName([FromQuery] string animalType, [FromQuery] bool? twoPart)
         {
-            return View();
+          
+            if (string.IsNullOrEmpty(animalType))
+            {
+                return BadRequest(new { error = "The 'animalType' query parameter is required." });
+            }
+
+            string animal = animalType.ToLower();
+            string[] names;
+
+            if (animal == "dog")
+            {
+                names = dogNames;
+            }
+            else if (animal == "cat")
+            {
+                names = catNames;
+            }
+            else if (animal == "bird")
+            {
+                names = birdNames;
+            }
+            else
+            {
+                return BadRequest(new { error = "Invalid 'animalType'." });
+            }
+
+
+            string generatePetName;
+            if (twoPart == true)
+            {
+                generatePetName = $"{names[_random.Next(names.Length)]} {names[_random.Next(names.Length)]}";
+            }
+            else
+            {
+                generatePetName = names[_random.Next(names.Length)];
+            }
+
+            return Ok(new { pet = generatePetName });
         }
     }
 }
+
