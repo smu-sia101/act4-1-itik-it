5cf0dcbed1804337ed0e7911a7596751381bb338
IanSalvador Ramos <IanSalvadorRamos23@outlook.com>
2025-03-25 23:17:24 -0800

    Initial code

---
+++ PetNameGenerator/PetNameGenerator/Controllers/PetNameGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetNameGenerator.Controllers
+{
+    [ApiController]
+    [Route("api/petnameGenerator")]
+    public class PetNameGenerator : ControllerBase
+    {
+        private string[] petnameGenerator = new string[]
+        {
+
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
